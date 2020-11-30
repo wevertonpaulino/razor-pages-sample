be3f86890a372e7f9d042aa768429dba63b3c830
Weverton <wevertoncesar@gmail.com>
2020-11-30 16:54:45 -0300

    Make scaffold

---
+++ Pages/Books/Edit.cshtml.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesBook.Data;
+using RazorPagesBook.Models;
+
+namespace RazorPagesBook.Pages_Books
+{
+    public class EditModel : PageModel
+    {
+        private readonly RazorPagesBook.Data.BookContext _context;
+
+        public EditModel(RazorPagesBook.Data.BookContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Book Book { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Book = await _context.Books.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Book == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Book).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BookExists(Book.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool BookExists(int id)
+        {
+            return _context.Books.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Pages/Books/Index.cshtml.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesBook.Data;
+using RazorPagesBook.Models;
+
+namespace RazorPagesBook.Pages_Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RazorPagesBook.Data.BookContext _context;
+
+        public IndexModel(RazorPagesBook.Data.BookContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Book = await _context.Books.ToListAsync();
+        }
+    }
+}
